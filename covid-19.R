e0489a7477d03cd5da69061fcd84435f3b07cf1d
Aaron Simumba <simumbaa21@gmail.com>
2020-05-27 09:43:33 +0200

    final code

--- covid-19.R
+++ covid-19.R
@@ -7,10 +7,9 @@
 # Copyright(c) Aaron Simumba
 # -------------------------------------
 
-library(covid19.analytics) # import the data
-library(tidyverse) # for data wrangling and manipulation
-library(lubridate) # date manipulation
-library(tidyr)
+library(covid19.analytics)
+library(tidyverse)
+library(lubridate)
 library(gganimate)
 theme_set(theme_minimal())
 
@@ -28,12 +27,12 @@ covid_tidy <- covid %>%
 zm_covid <- covid_tidy %>%
   filter(country_region == 'Zambia')
 
-zm_covid %>%
-  filter(confirmed_cases > 0) %>%
-  ggplot(aes(x = date_reported , y = confirmed_cases)) +
+zed_cases <- zm_covid %>%
+  filter(confirmed_cases != 0) %>%
+  ggplot(aes(x = date_reported , y = confirmed_cases, group = month)) +
   geom_line(size = 1, color = "#40004B") +
   geom_text(aes(x = as.Date('2020-05-19'),
-                label = round(x = as.numeric(confirmed_cases), digits = 0)),
+                label = as.factor(confirmed_cases)),
             hjust = 0) +
   transition_reveal(date_reported) +
   labs(title = 'Date: {frame_along}',
@@ -41,26 +40,37 @@ zm_covid %>%
        x = "Period",
        y = "Confirmed Cases" )
 
-anim_save("zambia_covid_19.gif")
+zed_cases %>%
+  animate() %>%
+anim_save(filename = "zambia_covid_19.gif")
 
+animate(zed_cases,
+        renderer = av_renderer("zed_cases.mp4"),
+        width = 1920, height = 1080,
+        res = 250, fps = 25)
+
+# All other countries
 country_colours <- c(Zambia = "#7F3B08",
                      Zimbabwe = "#A50026",
                      `South Africa` = "#40004B",
                      Botswana = "#276419")
 
-covid_tidy %>%
+final_all <- covid_tidy %>%
   filter(confirmed_cases > 0,
          country_region %in% c("Zambia",
                                "Zimbabwe",
                                "South Africa",
                                "Botswana")) %>%
-ggplot(aes(x = date_reported, y = confirmed_cases, colour = country_region)) +
+ggplot(aes(x = date_reported,
+           y = confirmed_cases,
+           colour = country_region,
+           group = month)) +
   geom_line(show.legend = TRUE, size = 1) +
   scale_colour_manual(values = country_colours) +
   scale_size(range = c(2, 12)) +
   facet_wrap(~ country_region, scales = "free_y") +
   geom_text(aes(x = as.Date('2020-05-19'),
-                label = round(x = as.numeric(confirmed_cases), digits = 0)),
+                label = as.factor(confirmed_cases)),
             hjust = 0, show.legend = FALSE) +
   labs(title = 'Date: {frame_along}',
        x = 'Period',
@@ -69,4 +79,11 @@ ggplot(aes(x = date_reported, y = confirmed_cases, colour = country_region)) +
        color = "Country") +
   transition_reveal(date_reported)
 
-anim_save("all_covid_19.gif")
+final_all %>%
+  animate() %>%
+  anim_save(filename = "final_all.gif")
+
+animate(zed_cases,
+        renderer = av_renderer("final_all.mp4"),
+        width = 1920, height = 1080,
+        res = 250, fps = 25)
